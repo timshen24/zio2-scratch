459902b0aa895e4338f3de2abe5482c662dd8118
tim.shen <tim.shen@farfetch.com>
2022-07-13 15:12:06 +0800

    upload variance problems and Variance positions

---
+++ src/main/scala/introduction/Variance.scala
@@ -0,0 +1,107 @@
+package introduction
+
+object Variance {
+  class Animal
+  class Dog(val name: String) extends Animal
+  trait Vet[-T] {
+    def heal(animal: T): Boolean
+  }
+
+  val myDog: Dog = Dog("Buddy")
+  val myVet: Vet[Dog] = (animal: Animal) => {
+    println(s"You will be fine, ${animal}")
+    true
+  }
+  myVet.heal(myDog)
+}
+
+object VariancePosition {
+  class Animal
+  class Dog extends Animal
+  class Cat extends Animal
+  class Crocodile extends Animal
+
+  class MyList[+T]
+  // if Dogs are Animals then is a MyList[Dog] also a MyList[Animal]
+  // the variance question
+
+  //  1 - yes => generic type is covariance
+  val anAnimal = new Dog
+  val animals: MyList[Animal] = new MyList[Dog]
+
+  //  2 - no => generic type is INVARIANT
+
+  //  3 - hell no! backwards => generic type is CONTRAVARIANT
+  class Vet[-T]
+  val lassiesVet: Vet[Dog] = new Vet[Animal]
+
+  // variance problem
+//  class MyList2[+T] {
+//    def head: T
+//    def tail: MyList2[T]
+//    def add(element: T): MyList2[T]
+//  }
+  /**
+   * The types of val fields are in COVARIANT position!
+   */
+  class Vet2[-T](val favoriteAnimal: T)
+  val garfield = new Cat
+  val theVet: Vet2[Animal] = new Vet2[Animal](garfield)
+  val lassiesVet2: Vet2[Dog] = theVet
+  val lassie: Dog = lassiesVet2.favoriteAnimal // type conflict now
+
+  /**
+   * The types of val fields are in COVARIANT position!
+   */
+  class Vet3[-T](var favoriteAnimal: T)
+
+  // types of var fields are ALSO in CONTRAVARIANT position
+  class MutableOption[+T](var contents: T)
+  val maybeAnimal: MutableOption[Animal] = new MutableOption[Dog](new Dog)
+  maybeAnimal.contents = new Cat // type conflict again
+
+  // type of method arguments
+  class MyList2[+T] {
+    def add(element: T): MyList2[T] = ???
+  }
+
+  val animals2: MyList2[Animal] = new MyList2[Cat]
+  val moreAnimals2 = animals2.add(new Dog) // type conflict again and again!
+
+  class VetAnother3[-T] {
+    def heal(animal: T): Boolean = true
+  }
+
+  val lassiesVetAnother3: VetAnother3[Dog] = new VetAnother3[Animal]
+  lassiesVetAnother3.heal(new Dog)
+//  lassiesVetAnother3.heal(new Cat) // legit error
+
+
+  /**
+   *   method return types are in COVARIANT positions
+   */
+  abstract class Vet4[-T] {
+    def rescueAnimal(): T
+  }
+
+  val vet4: Vet4[Dog] = new Vet4[Animal] {
+    override def rescueAnimal(): Animal = new Cat
+  }
+  vet4.rescueAnimal() // Obvious type conflict again!
+
+  /**
+   * Here you can think of S being Animal, while T being Cat or Dog
+   */
+  class MyListC[+T] {
+    def add[S >: T](element: S): MyListC[S] = new MyListC[S]
+  }
+
+  /**
+   * Here you can think of T being Animal, while S being Cat or Dog
+   */
+  class VetC[-T] {
+    def rescueAnimal[S <: T](): S = ???
+  }
+  val lassiesVetC: VetC[Dog] = new VetC[Animal]
+  val rescuedDogC: Dog = lassiesVetC.rescueAnimal() // rescueAnimal[Dog]
+}
---
+++ src/main/scala/introduction/ZIOExercise.scala
@@ -0,0 +1,57 @@
+package introduction
+
+import scala.io.StdIn._
+
+object ZIOExercise {
+  case class MyIO[A](unsafeRun:() => A) {
+    def map[B](f: A => B): MyIO[B] = {
+      MyIO(() => f(unsafeRun()))
+    }
+
+    def flatMap[B](f: A => MyIO[B]): MyIO[B] = {
+      MyIO(() => f(unsafeRun())).unsafeRun()
+    }
+  }
+
+  case class MyZIO[-R, +E, +A](unsafeRun: R => Either[E, A]) {
+    def map[B](f: A => B): MyZIO[R, E, B] = {
+      MyZIO(r => unsafeRun(r) match {
+        case Left(e) => Left(e)
+        case Right(v) => Right(f(v))})
+    }
+
+    def flatMap[R1 <: R, E1 >: E, B](f: A => MyZIO[R1, E1, B]): MyZIO[R1, E1, B] = {
+      MyZIO(r => unsafeRun(r) match {
+        case Left(e) => Left(e)
+        case Right(v) =>f(v).unsafeRun(r)})
+    }
+  }
+
+  def someDuration(): Unit = Thread.sleep(3000)
+
+  /**
+   * Create some IO which:
+   * 1. measure the current time of the system
+   * 2. measure the duration of a computation
+   *  - use exercise 1
+   * 3. read something from the console
+   * 4. print something to the console (e.g. "What's your name?"), then read, then print a welcome message
+   */
+  def main(args: Array[String]): Unit = {
+    val myIO1: MyIO[Long] = MyIO(() => System.currentTimeMillis())
+
+    val myIO2: MyIO[Long] = for {
+      startTime <- myIO1
+      _ <- MyIO(() => someDuration())
+      endTime <- myIO1
+    } yield endTime - startTime
+    println(myIO2.unsafeRun())
+
+    val myIO3With4: MyIO[Unit] = for {
+      _ <- MyIO(() => println("Please enter your name:"))
+      name <- MyIO[String](() => readLine())
+      _ <- MyIO(() => println(s"Welcome to ZIO2, $name"))
+    } yield ()
+    myIO3With4.unsafeRun()
+  }
+}
