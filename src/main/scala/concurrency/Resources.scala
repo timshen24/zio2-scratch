944919acdbe2b80e95953ff7fc6b4a89103a12b9
tim.shen <tim.shen@farfetch.com>
2022-08-10 13:16:12 +0800

    Resources.scala

---
+++ src/main/scala/concurrency/Resources.scala
@@ -0,0 +1,130 @@
+package concurrency
+
+import zio.*
+import utilsScala2.*
+
+import java.io.File
+import java.util.Scanner
+
+object Resources extends ZIOAppDefault {
+
+  // finalizers
+  def unsafeMethod(): Int = throw new RuntimeException("Not an int here for you")
+  val anAttempt = ZIO.attempt(unsafeMethod())
+
+  // finalizers
+  val attemptWithFinalizer = anAttempt.ensuring(ZIO.succeed("finalizer").debugThread)
+  // 2 finalizers
+  val attemptWith2Finalizer = attemptWithFinalizer.ensuring(ZIO.succeed("another finalizer!").debugThread)
+  // .onInterrupt, .onError, .onDone, .onExit
+
+  // manage resource Life cycle
+  class Connection(url: String) {
+    def open() = ZIO.succeed(s"opening connection to $url...").debugThread
+    def close() = ZIO.succeed(s"closing connection to $url").debugThread
+  }
+
+  object Connection {
+    def create(url: String) = ZIO.succeed(new Connection(url))
+  }
+
+  val fetchUrl = for {
+    conn <- Connection.create("rockthejvm.com")
+    fib <- (conn.open() *> ZIO.sleep(300.seconds)).fork
+    _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting").debugThread *> fib.interrupt
+    _ <- fib.join
+  } yield () // resource leak
+
+  val correctFetchUrl = for {
+    conn <- Connection.create("rockthejvm.com")
+    fib <- (conn.open() *> ZIO.sleep(300.seconds)).ensuring(conn.close()).fork
+    _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting").debugThread *> fib.interrupt
+    _ <- fib.join
+  } yield () // preventing leaks
+  // the above way is tedious
+
+
+  /*
+  Suggested:
+   acquireRelease
+    - acquiring cannot be interrupted
+    - all finalizers are guaranteed to run
+   */
+  val cleanConnection = ZIO.acquireRelease(Connection.create("rockthejvm.com"))(_.close())
+  val fetchWithResource = for {
+    conn <- cleanConnection
+    fib <- (conn.open() *> ZIO.sleep(300.seconds)).fork
+    _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting").debugThread *> fib.interrupt
+    _ <- fib.join
+  } yield ()
+
+  val fetchWithScopedResource = ZIO.scoped(fetchWithResource)
+
+  // acquireReleaseWith
+  val cleanConnection_v2 = ZIO.acquireReleaseWith(
+    Connection.create("rockthejvm.com") // acquire
+  )(
+    _.close() // release
+  )(
+    conn => conn.open() *> ZIO.sleep(300.seconds) // use
+  )
+
+  val fetchWithResource_v2 = for {
+    fib <- cleanConnection_v2.fork
+    _ <- ZIO.sleep(1.second) *> ZIO.succeed("interrupting").debugThread *> fib.interrupt
+    _ <- fib.join
+  } yield ()
+
+  /**
+   * Exercises
+   * 1. Use the acquireRelease to open a file, print all lines, (one every 100 millis), then close the file
+   */
+  def openFileScanner(path: String): UIO[Scanner] =
+    ZIO.succeed(new Scanner(new File(path)))
+
+  def readLineByLine(scanner: Scanner): UIO[Unit] =
+    if (scanner.hasNextLine)
+      ZIO.succeed(scanner.nextLine()).debugThread *> ZIO.sleep(100.millis) *> readLineByLine(scanner)
+    else
+      ZIO.unit
+
+  def acquireOpenFile(path: String): UIO[Unit] =
+    ZIO.succeed(s"opening file at $path").debugThread *>
+      ZIO.acquireReleaseWith(
+        openFileScanner(path) // acquire
+      )(
+        scanner => ZIO.succeed(s"closing file at $path").debugThread *> ZIO.succeed(scanner.close()) // close
+      )(
+        readLineByLine // usage
+      )
+
+  val testInterruptFileDisplay = for {
+    fib <- acquireOpenFile("src/main/scala/concurrency/Resources.scala").fork
+    _ <- ZIO.sleep(2.seconds) *> fib.interrupt
+  } yield ()
+
+  // acquireRelease vs acquireReleaseWith
+  def connFromConfig(path: String): UIO[Unit] =
+    ZIO.acquireReleaseWith(openFileScanner(path))(scanner => ZIO.succeed("closing file").debugThread *> ZIO.succeed(scanner.close())) { scanner =>
+      ZIO.acquireReleaseWith(Connection.create(scanner.nextLine()))(_.close()) { conn =>
+        conn.open() *> ZIO.never
+      }
+    }
+
+  // for nested resource, acquireRelease is obviously better!
+  def connFromConfig_v2(path: String): UIO[Unit] = ZIO.scoped { // notic the scoped here!
+    for {
+      scanner <- ZIO.acquireRelease(openFileScanner(path))(scanner => ZIO.succeed("closing file").debugThread *> ZIO.succeed(scanner.close()))
+      conn <- ZIO.acquireRelease(Connection.create(scanner.nextLine()))(_.close())
+      _ <- conn.open() *> ZIO.never
+    } yield ()
+  }
+
+  //  def run = attemptWith2Finalizer
+//  def run = fetchUrl // unsafe
+//  def run = correctFetchUrl // Safe
+//  def run = fetchWithResource // Safe
+//  def run = fetchWithResource_v2 // Safe
+//  def run = testInterruptFileDisplay
+  def run = connFromConfig_v2("...")
+}
